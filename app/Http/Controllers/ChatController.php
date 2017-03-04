853d65a4acdb6daa849cd344a36338a29c4834a8
Gasper07 <gasper0706@gmail.com>
2017-03-04 12:12:59 -0600

    cha2

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -6,8 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Request as facedesrequest;
 use Illuminate\Support\Facades\Redirect;
 
-require_once(app_path().'/pusher/Pusher.php');
-
 class ChatController extends Controller
 {
     /**
@@ -34,6 +32,7 @@ class ChatController extends Controller
         // dd($data);
         // Change the following with your app details:
         // Create your own pusher account @ https://app.pusher.com
+        require('http://app-fd8d1fda-4b1b-423f-aa23-358cd43f64b3.cleverapps.io/lib/Pusher.php');
         $app_id = '309483'; // App ID
         $app_key = '1699db0a3a3aec4d93c6'; // App Key
         $app_secret = '17a8b609741fa2891df9'; // App Secret

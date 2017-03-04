f03ff84d50ac231c0d4afa7db3e7072fe2a60bb0
Gasper07 <gasper0706@gmail.com>
2017-03-04 09:36:45 -0600

    vgr

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -137,12 +137,10 @@
  
     let pusher = new Pusher('{{ env("PUSHER_KEY") }}'),
         channel = pusher.subscribe('{{ $chatChannel }}');
-        sw = channel.bind('{{ new-message }}');
         console.log(pusher+'pusher');
         console.log(channel+'canal');
-        console.log(sw+'ws23');
  
-    channel.bind('new-message', addMessage('new-message'));
+    channel.bind('new-message', addMessage);
 </script>
  
 </body>

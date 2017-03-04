8556e8fee20d1aaa3e8e0cd8dc1dfe45d537ec1a
Gasper07 <gasper0706@gmail.com>
2017-03-04 12:35:13 -0600

    chavde

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -3,10 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Request as facedesrequest;
-use Illuminate\Support\Facades\Redirect;
 
-require __DIR__.'lib/Pusher.php';
 
 class ChatController extends Controller
 {
@@ -16,10 +13,10 @@ class ChatController extends Controller
      * @return void
      */
 
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     /**
      * Show the application dashboard.
@@ -28,17 +25,8 @@ class ChatController extends Controller
      */
 
     
-    public function index(Request $request)
+    public function index()
     {
-        $data = facedesrequest::all();
-        // Change the following with your app details:
-        // Create your own pusher account @ https://app.pusher.com
-        $app_id = '309483'; // App ID
-        $app_key = '1699db0a3a3aec4d93c6'; // App Key
-        $app_secret = '17a8b609741fa2891df9'; // App Secret
-        $pusher = new Pusher($app_key, $app_secret, $app_id);
-
-        dd($pusher);
         return view('usuarios.home');
     }
 

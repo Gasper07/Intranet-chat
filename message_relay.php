137a3550538977df9e2e2a180c11105a559ad4b3
Gasper07 <gasper0706@gmail.com>
2017-03-05 19:15:41 -0600

    more cha

--- message_relay.php
+++ message_relay.php
@@ -8,17 +8,23 @@ $app_key = '1699db0a3a3aec4d93c6'; // App Key
 $app_secret = '17a8b609741fa2891df9'; // App Secret
 $pusher = new Pusher($app_key, $app_secret, $app_id);
 
+//Open a new connection to the MySQL server
 $mysqli = new mysqli("btzb9icdm-mysql.services.clever-cloud.com", "u6v5e7r3xhfoadzq", "2ZHpKpA8lD9rvgrOIPX", "btzb9icdm");
+
+//Output any connection error
+if ($mysqli->connect_error) {
+    die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+}
 
 // Check the receive message
 if(isset($_POST['message']) && !empty($_POST['message'])) {		
 	$data['message'] = $_POST['message'];	
 	
 	// Return the received message
-	if($pusher->trigger('test_channel', 'my_event', $data)) {		
-		$mysqli->query("INSERT INTO chats_users ('id','conversations','id_user','created_at','updated_at') VALUES ('','prueba','5','','')");
-		prin_r("Se creó con éxtio ");
-		mysqli_close($mysqli);				
+	if($pusher->trigger('test_channel', 'my_event', $data)) {			
+		//MySqli Insert Query
+		$insert_row = $mysqli->query("INSERT INTO chats_users (id, conversations, id_user) VALUES('', 'que ondas', '55')");
+	
 		echo 'success';			
 	} else {		
 		echo 'error';	

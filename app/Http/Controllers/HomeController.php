670b141e38dfb2119ddf31f76feab9218927a139
Gasper07 <gasper0706@gmail.com>
2017-03-07 14:01:37 -0600

    t

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -75,10 +75,10 @@ class HomeController extends Controller
             $queryConversationUser = User::findOrFail($id_ForChat);
             // $ConversationBetwwenUser = Chats::where('id_user','=',$idUserLogin)->where('id_user_conversation','=',$id_ForChat);
 
-            $ConversationBetwwenUser = Chats::where('id_user','=',$idUserLogin)->orWhere('id_user_conversation', $idUserLogin)->get();
+            $ConversationBetwwenUser = Chats::where('id_user','=',$idUserLogin)->get();
 
             foreach ($ConversationBetwwenUser as $keyConversationBetwwenUser) {
-                if($keyConversationBetwwenUser->id_user_conversation == $id_ForChat or $keyConversationBetwwenUser->id_user_conversation == $idUserLogin){
+                if($keyConversationBetwwenUser->id_user_conversation == $id_ForChat){
                     /*Convertimos fechas*/
                     $date = new \Carbon\Carbon($keyConversationBetwwenUser->created_at);                    
                     $Fechas = $date->format('d-m-Y');

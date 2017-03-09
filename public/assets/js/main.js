ff638c70a497622d76b6ff55fd2c17ab817208c2
Gasper07 <gasper0706@gmail.com>
2017-03-09 08:53:23 -0600

    de

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -55,7 +55,7 @@ jQuery(document).ready(function($) {
 		    		
 		    	});
 
-		        console.log('almacenado3');
+		        console.log('almacenado3 nuevo');
 		    },
 		    error: function() {
 		        console.log('Error');

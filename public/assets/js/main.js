5d0ee050634f3e1dcd993ddb6f121fb896ead679
Gasper07 <gasper0706@gmail.com>
2017-03-09 08:48:59 -0600

    change url

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -18,11 +18,7 @@ jQuery(document).ready(function($) {
 		});
 
 		$.ajax({
-<<<<<<< HEAD
-		    url: 'http://127.0.0.1/sitios/Intranet-chat/chatUsers',
-=======
-		    url: 'http://127.0.0.1/Sites/Intranet-chat/chatUsers',
->>>>>>> origin/master
+		    url: 'http://app-fd8d1fda-4b1b-423f-aa23-358cd43f64b3.cleverapps.io/chatUsers',
 		    type: 'POST',
 		    headers: { 'X-CSRF-Token': $('input[name=_tokens]').attr('value') },
 		    data: "idForChat="+dataIdUser+"&_tokens=YIIXEDMNztyGoKqDrX7B9V20THP2hP0fAZFeiK3L",
@@ -59,7 +55,7 @@ jQuery(document).ready(function($) {
 		    		
 		    	});
 
-		        console.log('almacenado3 newvo');
+		        console.log('almacenado3');
 		    },
 		    error: function() {
 		        console.log('Error');
